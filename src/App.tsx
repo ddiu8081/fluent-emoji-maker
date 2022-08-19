3edc8249f6d0f036e872bfa43b61109d2bc59dee
ddiu8081 <ddiu8081@gmail.com>
2022-08-19 23:57:36 +0800

    feat: add details layer

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ const App: Component = () => {
   const [headImages, setHeadImages] = createSignal([]);
   const [eyesImages, setEyesImages] = createSignal([]);
   const [mouthImages, setMouthImages] = createSignal([]);
+  const [detailImages, setDetailImages] = createSignal([]);
+
   const [selectedHead, setSelectedHead] = createSignal(0);
   const [selectedHeadImage, setSelectedHeadImage] = createSignal('');
   const [selectedEyes, setSelectedEyes] = createSignal(0);
   const [selectedEyesImage, setSelectedEyesImage] = createSignal('');
   const [selectedMouth, setSelectedMouth] = createSignal(0);
-  const [selectedMouthImage, setSelectedMouthImage] = createSignal('');  
+  const [selectedMouthImage, setSelectedMouthImage] = createSignal('');
+  const [selectedDetail, setSelectedDetail] = createSignal(0);
+  const [selectedDetailImage, setSelectedDetailImage] = createSignal('');
 
   const loadImage = async () => {
     // head
@@ -30,6 +34,12 @@ const App: Component = () => {
     const mouthValues = Object.values(mouthModules).map(m => m())
     const fullMouthImages = await Promise.all(mouthValues)
     setMouthImages(fullMouthImages)
+
+    // detail
+    const detailModules = await import.meta.glob('./assets/details/*.svg')
+    const detailValues = Object.values(detailModules).map(m => m())
+    const fullDetailImages = await Promise.all(detailValues)
+    setDetailImages(fullDetailImages)
   }
   loadImage()
 
@@ -46,12 +56,16 @@ const App: Component = () => {
     setSelectedMouth(i)
     setSelectedMouthImage(mouthImages()[i()].default)
   }
+  const handleClickDetail = (i) => {
+    setSelectedDetail(i)
+    setSelectedDetailImage(detailImages()[i()].default)
+  }
 
   return (
     <>
       <h1 text="2xl" font="bold">Fluent Emoji Maker</h1>
 
-      <div mt-4 flex="~ row" gap-2>
+      <div mt-4 flex="~ row wrap" gap-2>
         <For each={headImages()}>
           {(item, index) => (
             <SelectButton >
@@ -60,7 +74,7 @@ const App: Component = () => {
           )}
         </For>
       </div>
-      <div mt-4 flex="~ row" gap-2>
+      <div mt-8 flex="~ row wrap" gap-2>
         <For each={eyesImages()}>
           {(item, index) => (
             <SelectButton >
@@ -69,7 +83,7 @@ const App: Component = () => {
           )}
         </For>
       </div>
-      <div mt-4 flex="~ row" gap-2>
+      <div mt-8 flex="~ row wrap" gap-2>
         <For each={mouthImages()}>
           {(item, index) => (
             <SelectButton >
@@ -78,11 +92,21 @@ const App: Component = () => {
           )}
         </For>
       </div>
+      <div mt-8 flex="~ row wrap" gap-2>
+        <For each={detailImages()}>
+          {(item, index) => (
+            <SelectButton >
+              <img onClick={[handleClickDetail, index]} src={item.default} alt=""></img>
+            </SelectButton>
+          )}
+        </For>
+      </div>
 
       <div mt-8 border h-32>
-        <img  w-24 h-24 src={ selectedHeadImage() } />
-        <img  w-24 h-24 src={ selectedEyesImage() } />
-        <img  w-24 h-24 src={ selectedMouthImage() } />
+        <img class="absolute" w-24 h-24 src={ selectedHeadImage() } />
+        <img class="absolute" w-24 h-24 src={ selectedEyesImage() } />
+        <img class="absolute" w-24 h-24 src={ selectedMouthImage() } />
+        <img class="absolute" w-24 h-24 src={ selectedDetailImage() } />
       </div>
     </>
   );
