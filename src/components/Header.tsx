881c67a83866c85e35832ccc80515aa659755929
starknt <1431880400@qq.com>
2022-08-21 12:01:22 +0800

    feat: dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,38 @@
+import { createEffect } from "solid-js"
+import { isDark, setDark } from "../logic"
+
 export default () => {
+  createEffect(() => {
+    const node = document.querySelector('html')
+
+    if (isDark()) {
+      node?.classList.add('dark')
+      localStorage.setItem('dark', '1')
+    } else {
+      node?.classList.remove('dark')
+      localStorage.removeItem('dark')
+    }
+  })
+
+  const handleToggleDark = () => setDark((prev) => !prev)
+
   return (
-    <header>
-      
+    <header flex="~">
+      <div
+        right-8
+        pos-absolute
+        inline-flex justify-center items-center
+        cursor-pointer
+        p-2 rounded-full op-90
+        hover:op-50
+        dark:text-white dark:op-50
+        hover="dark:op-90"
+        onClick={handleToggleDark}
+      >
+        {
+          isDark() ? <i text-2xl i-carbon:sun /> : <i text-2xl i-carbon:moon />
+        }
+      </div>
     </header>
   )
 }
